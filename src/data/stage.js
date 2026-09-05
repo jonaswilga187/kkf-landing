4b808c2b90ccfe97cbe0525f8b9b833ea5787736
Jonas Wilga <jonaswilga187@gmail.com>
2026-09-05 13:27:46 +0200

    Stadtkantorei-Liedtext-PDF ab sofort live verlinkt
    Freigabe des Künstlers/der Stadtkantorei vorgezogen, analog zu Fritz
    Baltruweit. Geplante Löschung am 5.9. um 16:55 Uhr bleibt bestehen.

--- src/data/stage.js
+++ src/data/stage.js
@@ -48,6 +48,7 @@ Nun danket alle Gott
 Jesu bleibet meine Freude
 Halleluja (aus: Der Messias)
 Verleih uns Frieden gnädiglich`,
+    lyricsFile: '/lyrics/stadtkantorei-singalong.pdf',
   },
   {
     id: 'fritz-baltruweit',
